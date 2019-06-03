3c80a873d2d2962062f64feddc52129865fe18cf
Irshad Ansari <irshadjsr21@gmail.com>
2019-06-03 16:49:22 +0530

    [Feat] Added header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<div class="header-container">
+  <div class="container">
+    <div class="header">
+      <div class="col-sm-8 col-12">
+        <a href="#" class="header-link">About</a>
+        <a href="#" class="header-link">Contact</a>
+        <a href="#" class="header-link">Sign up</a>
+        <a href="#" class="header-link">Login</a>
+      </div>
+      <div class="col-sm-4 col-12">
+        <div class="icon-container">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ resources/views/components/topbar.blade.php
@@ -0,0 +1,28 @@
+<div class="topbar-container">
+  <div class="container">
+    <div class="topbar">
+      <div class="topbar-header">
+        <input type="checkbox" id="nav-toggle" name="nav-toggle" class="nav-toggle-input">
+        <div class="logo">
+          <h1><span class="text-primary font-weight-bold">Matrix</span> Feed</h1>
+        </div>
+
+        <label for="nav-toggle" class="nav-toggle">
+          <i class="fas fa-bars"></i>
+        </label>
+
+        <div class="ad-space">
+          
+        </div>
+
+        <nav class="topbar-nav">
+          <a href="#">Home</a>
+          <a href="#">Home</a>
+          <a href="#">Home</a>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</div>
