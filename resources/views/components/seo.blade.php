473da732fb5519386f5461a000f25b3d66ee8172
Irshad Ansari <irshadjsr21@gmail.com>
2019-07-28 16:48:49 +0530

    [Fix] Fixed seo html error

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -11,23 +11,23 @@
 
 @if ($SEO['description'])
   <meta name="description" itemprop="description" content="{{ $SEO["description"] }}" />
-  <meta property="og:description"content="{{ $SEO["description"] }}" /> 
+  <meta property="og:description" content="{{ $SEO["description"] }}" /> 
 @endif
 
 @if ($SEO['image'])
-  <meta property="og:image"content="{{ $SEO['image'] }}" />
-  <meta name="twitter:image" content="{{ $SEO['image'] }}"> 
+  <meta property="og:image" content="{{ $SEO['image'] }}" />
+  <meta name="twitter:image"  content="{{ $SEO['image'] }}"> 
 @endif
 
 @if(!$SEO['isMinimal'])
-  <meta property="og:title"content="{{ $SEO['title'] }}" />
-  <meta property="og:url"content="{{ $SEO['url'] }}" />
-  <meta property="og:type"content="{{ $SEO['type'] }}" />
-  <meta property="og:site_name"content="{{ $SEO['siteName'] }}" />
-  <meta property="fb:app_id" content="{{ $SEO['fbId'] }}" />
+  <meta property="og:title" content="{{ $SEO['title'] }}" />
+  <meta property="og:url" content="{{ $SEO['url'] }}" />
+  <meta property="og:type" content="{{ $SEO['type'] }}" />
+  <meta property="og:site_name" content="{{ $SEO['siteName'] }}" />
+  <meta property="fb:app_id"  content="{{ $SEO['fbId'] }}" />
 
-  <meta name="twitter:card"content="summary" />
-  <meta name="twitter:title"content="{{ $SEO['title'] }}" />
-  <meta name="twitter:site"content="{{ $SEO['twitterUsername'] }}" />
-  <meta name="twitter:creator" content="{{ $SEO['twitterUsername'] }}"/>
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content="{{ $SEO['title'] }}" />
+  <meta name="twitter:site" content="{{ $SEO['twitterUsername'] }}" />
+  <meta name="twitter:creator"  content="{{ $SEO['twitterUsername'] }}"/>
 @endif
